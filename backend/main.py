9c1aaa853098c16c244a122ed6c8f00e1b086812
mattiviera <matiiviera38@gmail.com>
2024-09-10 21:41:35 -0300

    correccion de error en routes

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ from config.cnx import engine
 
 # Ruotes
 from default.routes import default
-from animalitos.routes import animales
+from animalitos.routes import animal
 #from task.routes import task
 
 app = FastAPI(
@@ -23,7 +23,7 @@ app = FastAPI(
 
 # Ruotes Apps
 app.include_router(default, prefix='', tags=['App Routes Default'])
-app.include_router(animales, prefix='/animales', tags=['Users Endpoints'])
+app.include_router(animal, prefix='/animales', tags=['Users Endpoints'])
 #app.include_router(task, prefix='/task', tags=['Tasks Endpoints'])
 
 Base.metadata.create_all(bind=engine)
